ef1f261a37d5113a15a34f766d8031f2f11e0417
abstrakcja <78209792+emotikonek@users.noreply.github.com>
2024-03-13 15:57:33 +0100

    Add files via upload

--- praktyki zad 2/dodawanieproduktow.php
+++ praktyki zad 2/dodawanieproduktow.php
@@ -44,6 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dodawanie produktu</title>
+    <link rel="stylesheet" href="cssdodawanie.css">
 </head>
 <body>
     <h2>Dodaj nowy produkt</h2>
