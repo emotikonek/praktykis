771ff6975f654e833501bc4e63b0ade9fa40c728
abstrakcja <78209792+emotikonek@users.noreply.github.com>
2024-03-18 19:11:09 +0100

    Update pogoda.php

--- pogoda.php
+++ pogoda.php
@@ -14,36 +14,33 @@
 <!-- e719de05a990ab676288b93baa4e8fab -->
 <?php
 
-
 $city = "London"; 
 $api_key = "e719de05a990ab676288b93baa4e8fab";
-
-
 $url = "https://api.openweathermap.org/data/2.5/weather?q=" . urlencode($city) . "&appid=" . $api_key;
 
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
 
-$weather_data = json_decode(file_get_contents($url), true);
+if(curl_errno($ch)){
+    echo 'Curl error: ' . curl_error($ch);
+}
 
+curl_close($ch);
+
+$weather_data = json_decode($response, true);
 
 $temperature = $weather_data['main']['temp'];
-
 $temperature_in_celcius = round($temperature - 273.15);
 
 $feels_like = $weather_data['main']['feels_like'];
-
 $feels_like_in_celcius = round($feels_like - 273.15);
 
-
 $humidity = $weather_data['main']['humidity'];
-
 $windspeed = $weather_data['wind']['speed'];
 
-
-
 $temperature_current_weather_ = $weather_data['weather'][0]['main'];
-
 $temperature_current_weather_description = $weather_data['weather'][0]['description'];
-
 $temperature_current_weather_icon = $weather_data['weather'][0]['icon'];
 
 $data=date("Y-m-d");
@@ -66,15 +63,6 @@ echo "</br>";
 echo "ikona pogody: <img src='http://openweathermap.org/img/wn/" .$temperature_current_weather_icon."@2x.png' />";
 echo "</br>";
 
- echo "<pre>";
+echo "<pre>";
 print_r($weather_data); 
-
-
-
-
-
-
-
-
-
 ?>
